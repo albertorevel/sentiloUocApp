03dd3202b71c4641e4d06ffe1e3012349bfa628c
Alberto Revel <albertorevel@users.noreply.github.com>
2019-05-14 21:15:46 +0200

    model location changed v2

---
+++ src/app/model/sensorType.ts
@@ -0,0 +1,34 @@
+export class SensorType {
+
+    private _id: string;
+    private _name: string;
+    private _newId: string;
+
+    constructor (id: string) {
+        this.id = id;
+        this.name = '';
+        this.newId = '';
+    }
+    
+    public get id(): string {
+        return this._id;
+    }
+    public set id(value: string) {
+        this._id = value;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+
+    public get newId(): string {
+        return this._newId;
+    }
+    public set newId(value: string) {
+        this._newId = value;
+    }
+    
+}
